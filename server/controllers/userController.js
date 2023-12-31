f9b516b3bc63e179d5bdfc07fcd3c6d41b1b6e6e
Valentin Roehle <roehlevalentin@gmail.com>
2023-12-31 19:38:55 +0100

    Update server configuration and user controller
    Add error middleware and user routes
    
    Add express-async-handler dependency

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,32 @@
-const authUser = (req, res) => {
-  res.send(200).json({ message: "Auth User" });
-};
+import asyncHandler from "express-async-handler";
 
-export { authUser };
+// @desc AUTH user/set token
+// route POST /api/users/auth
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "get User profile" });
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -7,6 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => res.send("Server is ready."));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
