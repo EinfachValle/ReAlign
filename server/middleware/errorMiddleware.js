44f22da3193fa66e5729ecb6fffa2008b5b88b0d
Valentin Roehle <roehlevalentin@gmail.com>
2024-01-05 20:46:14 +0100

    Refactor error handling in errorMiddleware.js

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -5,18 +5,20 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  let message = err.message;
+  if (!res.headersSent) {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
 
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    statusCode = 404;
-    message = "Resource not found";
+    if (err.name === "CastError" && err.kind === "ObjectId") {
+      statusCode = 404;
+      message = "Resource not found";
+    }
+
+    res.status(statusCode).json({
+      message,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
   }
-
-  res.status(statusCode).json({
-    message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
 };
 
 export { notFound, errorHandler };
